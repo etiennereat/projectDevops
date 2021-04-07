4294bfdbd6d3b1ae3b0fdf428512c671f9bba337
Nicu Vrabii <nicuvrabii1@gmail.com>
2021-04-07 10:40:43 +0200

    Initiates Maven project

---
+++ src/main/java/com/panda/app/App.java
@@ -0,0 +1,13 @@
+package com.panda.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
