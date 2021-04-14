97d65b252bfa8d21fb93c6f0809840dcc9f7fe25
Nicu Vrabii <nicuvrabii1@gmail.com>
2021-04-14 18:28:36 +0200

    Updates packages hierarchies

--- src/main/java/com/panda/dataframe/DataFrame.java
+++ src/main/java/com/panda/dataframe/DataFrame.java
@@ -1,5 +1,7 @@
 package com.panda.dataframe;
 
+import com.panda.datacol.DataCol;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,67 +9,67 @@ public class DataFrame {
 
     ArrayList<String> labels;
     ArrayList<String> indexes;
-    HashMap<String,DataCol> table;
+    HashMap<String, DataCol> table;
 
-    public DataFrame(){
+    public DataFrame() {
         this.labels = new ArrayList<String>();
-        this.table = new HashMap<String,DataCol>();
+        this.table = new HashMap<String, DataCol>();
         this.indexes = new ArrayList<String>();
 
     }
 
-    public boolean addCol(String label,DataCol dl){
-        if(!table.containsKey(label)){
+    public boolean addCol(String label, DataCol dl) {
+        if (!table.containsKey(label)) {
             this.labels.add(label);
-            this.table.put(label,dl);
+            this.table.put(label, dl);
             updateIndexes(dl.getIndexes());
             return true;
         }
         return false;
     }
 
-    public boolean addCol(DataCol dl){
-        return addCol(Integer.toString(dl.hashCode()),dl);
+    public boolean addCol(DataCol dl) {
+        return addCol(Integer.toString(dl.hashCode()), dl);
     }
 
-    private void updateIndexes(ArrayList<String> indexes){
+    private void updateIndexes(ArrayList<String> indexes) {
         //Todo
     }
 
-    public boolean removeCol(String label){
-        //Todo
-        return false;
-    }
-
-    public boolean removeRow(String index){
+    public boolean removeCol(String label) {
         //Todo
         return false;
     }
 
-    public DataCol getColsCount(String label){
+    public boolean removeRow(String index) {
+        //Todo
+        return false;
+    }
+
+    public DataCol getColsCount(String label) {
         //Todo
         return null;
     }
 
-    public int getColsCount(){
+    public int getColsCount() {
         return table.size();
     }
 
-    public ArrayList<String> getIndexes(){
+    public ArrayList<String> getIndexes() {
         //Todo
         return null;
     }
 
-    public ArrayList<String> getLabels(){
-        return (ArrayList<String>) labels.clone();
+    public ArrayList<String> getLabels() {
+        return new ArrayList<String>(labels);
     }
 
-    public void show(){
+    public void show() {
         //Todo
     }
 
 
-    public void show (String index) {
+    public void show(String index) {
         //Todo
     }
 
